71c10079355fd0d5ae8e35dcf150a134f33819f6
Raúl Padilla Delgado <padilladelgadoraul@gmail.com>
2024-04-15 23:22:46 +0100

    refactor - reorganize methods and extract Markdown link format logic

--- src/main/kotlin/MarkdownTransformer.kt
+++ src/main/kotlin/MarkdownTransformer.kt
@@ -1,27 +1,9 @@
 class MarkdownTransformer {
-    companion object {
-        private val LINK_REGEX = "\\[(.*?)]\\((.*?)\\)".toRegex()
-    }
-
-    fun transform(markdown: String): String {
-        val links = findLinksAt(markdown)
-        val transformedText = replaceLinksByAnchors(markdown, links)
-        val anchors = associateAnchorsToLinks(links)
-        return appendAnchorsToText(transformedText, anchors)
-    }
-
-    private fun appendAnchorsToText(text: String, anchors: Map<Int, String>): String {
-        if (anchors.isEmpty()) {
-            return text
-        }
-        val anchorsText = anchors.entries.joinToString(separator = "\n") { "[${it.key}]: ${it.value}" }
-        return "$text\n$anchorsText"
-    }
-
-    private fun replaceLinksByAnchors(markdownText: String, links: List<MarkdownLink>): String {
-        return links.fold(markdownText) { acc, link ->
-            acc.replace("[${link.name}](${link.url})", "${link.name} [${links.indexOf(link) + 1}]")
-        }
+    fun transform(textWithLinks: String): String {
+        val links = findLinksAt(textWithLinks)
+        val textWithAnchors = replaceLinksByAnchors(textWithLinks, links)
+        val anchorsToLinks = associateAnchorsToLinks(links)
+        return appendAnchorsToText(textWithAnchors, anchorsToLinks)
     }
 
     private fun findLinksAt(markdownText: String): List<MarkdownLink> {
@@ -31,11 +13,35 @@ class MarkdownTransformer {
             .toList()
     }
 
+    private fun replaceLinksByAnchors(markdownText: String, links: List<MarkdownLink>): String {
+        return links.fold(markdownText) { acc, link ->
+            acc.replace(link.format(), link.formatAsAnchor(links.indexOf(link) + 1))
+        }
+    }
+
     private fun associateAnchorsToLinks(links: List<MarkdownLink>) =
         links.foldIndexed(mutableMapOf<Int, String>()) { index, acc, link ->
             acc[index + 1] = link.url
             acc
         }
+
+    private fun appendAnchorsToText(text: String, anchors: Map<Int, String>): String {
+        if (anchors.isEmpty()) {
+            return text
+        }
+        val anchorsText = anchors.entries.joinToString(separator = "\n") { "[${it.key}]: ${it.value}" }
+        return "$text\n$anchorsText"
+    }
 }
 
-data class MarkdownLink(val name: String, val url: String)
+private val LINK_REGEX = "\\[(.*?)]\\((.*?)\\)".toRegex()
+
+data class MarkdownLink(val name: String, val url: String) {
+    fun format(): String {
+        return "[${name}](${url})"
+    }
+
+    fun formatAsAnchor(anchorNumber: Int): String {
+        return "$name [$anchorNumber]"
+    }
+}
